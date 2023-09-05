de5857dbca02ea62d7def2d99bf8d242a148c6dc
양현성 <didgustjd31349@naver.com>
2023-09-05 19:06:50 +0900

    양현성: 상세페이지 우측 디자인 완료

--- andcrowd/src/pages/and/AndDetail.js
+++ andcrowd/src/pages/and/AndDetail.js
@@ -3,15 +3,22 @@ import { redirect, useParams } from 'react-router-dom';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from "axios";
 import AndToolbar from "../../components/and/AndToolBar";
+import Typography from '@mui/material/Typography';
+import { AiOutlineHeart  ,AiFillHeart} from "react-icons/ai";
+import CountdownTimer from "../../components/and/CountdownTimer";
+import Box from '@mui/material/Box';
+import '../../styles/and/AndDetail.css';
 
 const AndDetail = () => {
   const params = useParams();
   const andId = params.andId;
 
   const navigate = useNavigate();
-
+  const [isClicked, setIsClicked] = useState(false);
   const [and, setAnd] = useState({});
-
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
   useEffect(() => {
     fetchData();
   }, [andId]);
@@ -58,7 +65,6 @@ const AndDetail = () => {
   const andChat = (andId) => {
     navigate(`/and/${andId}/chat`);
   };
-
   if (and.deleted === true) {
     alert("이 글은 삭제되었습니다.");
     window.location.href = `/and/list`;
@@ -69,9 +75,27 @@ const AndDetail = () => {
     <div>
       <AndToolbar andId={and.andId} />
       <div> 
-        <h4>제목: {and.andTitle}</h4>
-        <p>본문: {and.andContent}</p>
-        <p>마감일: {and.andEndDate}</p>
+        <Box id='right-top-box'>
+          <Typography id ='and-title'>{and.andTitle}</Typography>
+          <CountdownTimer publishedAt={and.publishedAt} andEndDate={and.andEndDate} />
+          <hr style={{ margin: '20px auto', width: '70%' }}></hr>
+          <Box id='like-and-button'>
+            <Box id ='like-icon' onClick={handleClick}> 
+            {isClicked ? <AiFillHeart id='heart-icon' size={"30"}/> : <AiOutlineHeart id='heart-icon' size={"30"}/>}
+            </Box>
+            <button id='go-and'>
+              모임 참가하기
+            </button>
+            </Box>
+          <button id='go-chat'>
+            채팅방으로 이동하기
+          </button>
+          <Typography id ='go-member'>
+            모임에 참여중인 멤버 보기
+          </Typography>
+        </Box>
+        <Box id ='left-main-box'>
+        <Typography id ='and-content'>{and.andContent}</Typography>
         <button onClick={() => updateAnd(and.andId)}>edit</button>
         <button onClick={() => deleteAnd(and.andId)}>delete</button>
         <br />
@@ -79,8 +103,8 @@ const AndDetail = () => {
         <button onClick={() => applicantList(and.andId)}>apply List</button>
         <br />
         <button onClick={() => andChat(and.andId)}>CHAT</button>
-        <hr />
         <br />
+        </Box>
       </div>
       
     </div>
