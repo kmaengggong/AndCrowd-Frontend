c65d993f3d8a7d314f2c234b2828deba7cdbf90e
양현성 <didgustjd31349@naver.com>
2023-09-07 20:22:06 +0900

    양현성:Qna 임시디자인 완료

--- andcrowd/src/pages/and/AndQna.js
+++ andcrowd/src/pages/and/AndQna.js
@@ -5,39 +5,40 @@ import { Link } from 'react-router-dom';
 import AndToolBar from "../../components/and/AndToolBar";
 import { Typography } from "@mui/material";
 import '../../styles/and/AndQna.css'
-
 const AndQna = () => {
     const params = useParams();
     const andId = params.andId;
-    const andQnaId = params.andQnaId;
-    console.log(andQnaId);
     const [andQnaList, setAndQnaList] = useState([]);
     const [selectedQnaId, setSelectedQnaId] = useState(null);
     const [andReplyList, setAndReplyList] = useState({});
-
-    const handleQnaClick = (qnaId) => {
+    const handleQnaClick = (qnaId,andQna) => {
       if (selectedQnaId === qnaId) {
+        fetchReplyData(andId, qnaId)
         setSelectedQnaId(null);
       } else {
+        fetchReplyData(andId, qnaId)
         setSelectedQnaId(qnaId);
       }
     };
-
     useEffect(() => {
       fetchData();
-    }, [andId, andQnaId]);
-    
+    }, [andId]);
     const fetchData = async () => {
       try {
         const response = await fetch(`/and/${andId}/qna/list`);
-        
         if (response.ok) {
           const data = await response.json();
           setAndQnaList(data);
         } else {
           throw new Error(`Fetching and data failed with status ${response.status}.`);
         }
-    
+      } catch (error) {
+        console.error("Error fetching And data:", error);
+      }
+    };
+    const fetchReplyData = async (andId, andQnaId) => {
+      console.log("andQnaId: ", andQnaId)
+      try {
         const qnaReplyResponse = await fetch(`/and/${andId}/qna/reply/${andQnaId}/all`);
         if (qnaReplyResponse.ok) {
           const qnaReplyData = await qnaReplyResponse.json();
@@ -45,13 +46,10 @@ const AndQna = () => {
         } else {
           throw new Error(`Fetching and data failed with status ${qnaReplyResponse.status}.`);
       }
-  
       } catch (error) {
         console.error("Error fetching And data:", error);
       }
     };
-  
-
     return (
       <div id='qna-container'>
         <div id='qna-box'>
@@ -60,7 +58,7 @@ const AndQna = () => {
             <div key={andQna.andQnaId}>
               <Typography id='and-qna-title' display="inline">
                 <div
-                  onClick={() => handleQnaClick(andQna.andQnaId)}
+                  onClick={() => handleQnaClick(andQna.andQnaId)} 
                   style={{
                     cursor: 'pointer'
                   }}
@@ -70,11 +68,13 @@ const AndQna = () => {
               </Typography>
               <Typography>{andQna.updatedAt}</Typography>
               {selectedQnaId === andQna.andQnaId && (
-                <div id='qna-div'>
+                <div id='qna-div'
+                  style={{
+                    cursor: 'pointer'
+                  }}>
                   <h2 id='qna-content'>{andQna.andQnaContent}</h2>
-                  <p>{andQna.andQnaId}</p>
+                  <p>andQnaId: {andQna.andQnaId}</p>
                 </div>
-                
               )}
               {selectedQnaId === andQna.andQnaId && (
                 <div id='qna-div'>
@@ -87,7 +87,6 @@ const AndQna = () => {
                         <p>답변내용: {comment.andReplyContent}</p>
                         <p>답변자: {comment.userId}</p>
                         <p>마지막시간: {comment.updatedAt}</p>
-                        
                       </div>
                     ))
                   ) : (
@@ -95,16 +94,12 @@ const AndQna = () => {
                   )}
                   <hr id='and-qna-line'></hr>
                 </div>
-                
               )}
-              
             </div>
           ))}
         </div>
-  
         <Link to={`/and/${andId}/qna/create`}>글 작성</Link>
       </div>
     );
 };
-
 export default AndQna;
