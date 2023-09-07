0d14120f28fe91e073866b8fa7c25cb05ae61f86
임규빈 <gyublayla@gmail.com>
2023-09-07 15:35:26 +0900

    임규빈: 모임 검색 추가

--- andcrowd/src/App.js
+++ andcrowd/src/App.js
@@ -54,7 +54,7 @@ import ScrollToTop from './components/ScrollToTop';
 import AndApplicantAdmin from './pages/and/AndApplicantAdmin';
 import { NaverLoginCallback } from './components/sign/NaverLoginCallback';
 import AndManage from './pages/and/AndManage';
-import CrowdDetail from './pages/crowd/detail';
+import CrowdDetail from './pages/crowd/CrowdDetail';
 import CrowdList from './pages/CrowdList';
 import CrowdCreate from './pages/crowd/CrowdCreate';
 
@@ -155,7 +155,7 @@ function App() {
             <Route path="/crowd/:crowdId/board/all" element={<CrowdBoardList />} />
             <Route path="/crowd/:crowdId/board/:crowdBoardId" element={<CrowdBoardDetail />} />
             <Route path="/crowd/:crowdId/insert" element={<CrowdBoardInsert />} />
-            <Route path="/crowd/:crowdId/reward/:rewardId/payment" element={<CrowdPayment />} />
+            {/* <Route path="/crowd/:crowdId/reward/:rewardId/payment" element={<CrowdPayment />} /> */}
             <Route path="/crowd/detail/:crowdId" element={<CrowdDetail />} />
             <Route path="/crowd/:crowdId/reward/:rewardId/payment" element={<CrowdPaymentFunction />} />
             <Route path=":projectType/:projectId/ad/payment" element={<AdPaymentFunction />} />
