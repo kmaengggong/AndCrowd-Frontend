d984c4b7a5a919f0c992ecc92c9c96f128eb5d99
임규빈 <gyublayla@gmail.com>
2023-09-23 11:56:43 +0900

    임규빈: 펀딩 관리 페이지 추가

---
+++ andcrowd/src/pages/crowd/CrowdManage.js
@@ -0,0 +1,290 @@
+import React, { useState, useEffect } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import { useParams } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, MenuItem, FormControl, Select } from "@mui/material";
+
+const columns = [
+    { 
+        field: 'purchaseId', 
+        headerName: 'ID', 
+        width: 80
+    },
+    {
+        field: 'userId', 
+        headerName: '유저 ID',
+        width: 80
+    },
+    {
+        field: 'rewardName', 
+        headerName: '리워드명',
+        width: 150
+    },
+    {
+        field: 'purchaseAmount', 
+        headerName: '결제 금액',
+        width: 100
+    },
+    {
+        field: 'purchaseStatus', 
+        headerName: '주문 상태',
+        width: 100
+    },
+    {
+        field: 'purchaseDate', 
+        headerName: '결제일',
+        width: 120
+    },
+    {
+        field: 'purchaseName', 
+        headerName: '배송자명',
+        width: 90
+    },
+    {
+        field: 'purchasePhone', 
+        headerName: '전화번호',
+        width: 130
+    },
+    {
+        field: 'purchaseAddress', 
+        headerName: '배송주소',
+        width: 150
+    },
+];
+
+
+const CrowdManage = () => {
+    const navigate = useNavigate();
+    const params = useParams();
+    const crowdId = params.crowdId;
+
+    const [totalFunded, setTotalFunded] = useState(0);
+    const [crowd, setCrowd] = useState({});
+    const [rewardFundedList, setRewardFundedList] = useState([]);
+    const [purchaseList, setPurchaseList] = useState([]);
+    const [rowSelectionModel, setRowSelectionModel] = useState([]);
+    const [openProcessDialog, setOpenProcessDialog] = useState(false);
+    const [newStatus, setNewStatus] = useState('');
+
+    const NoRowsOverlay = () => (<p>비어있습니다.</p>);
+
+    useEffect(() => {
+        fetchCrowd();
+        fetchTotalFunded();
+        fetchRewardFunded();
+        fetchPurchase();
+    },[]);
+
+    useEffect(() => {
+    }, [rowSelectionModel]);
+
+
+    const fetchTotalFunded = async () => {
+        console.log(`/crowd_order/${crowdId}/total`);
+        try{
+            const response = await fetch(`/crowd_order/${crowdId}/total`);
+            if(response.ok){
+                const data = await response.json();
+                console.log("fetchTotalFunded: ", data)
+                setTotalFunded(data);
+            } else {
+                throw new Error(`Error fetchTotalFunded ${response.status} `);
+            }
+        } catch (error) {
+          console.error("Error fetchTotalFunded data:", error);
+        }
+    };
+
+    const fetchCrowd = async () => {
+        try{
+            const response = await fetch(`/crowd/${crowdId}`);
+            if(response.ok){
+                const data = await response.json();
+                console.log("fetchCrowd: ", data)
+                setCrowd(data);
+            } else{
+                throw new Error(`fetchCrowd error ${response.status} `);
+            }
+        } catch (error) {
+          console.error("Error fetching Crowd data:", error);
+        }
+    };
+
+    const fetchRewardFunded = async () => {
+        console.log(`/crowd_order/${crowdId}/reward`);
+        try{
+            const response = await fetch(`/crowd_order/${crowdId}/reward`);
+            if(response.ok){
+                const data = await response.json();
+                console.log("fetchRewardFunded:", data)
+                setRewardFundedList(data);
+            } else {
+                throw new Error(`Error fetchTotalFunded ${response.status} `);
+            }
+        } catch (error) {
+          console.error("Error fetchTotalFunded data:", error);
+        }
+    };
+
+    const fetchPurchase = async () => {
+        try{
+            const response = await fetch(`/crowd_order/${crowdId}/list`);
+            if(response.ok){
+                const data = await response.json();
+                console.log("fetchPurchase: ", data)
+                setPurchaseList(data);
+            } else {
+                throw new Error(`Error fetchTotalFunded ${response.status} `);
+            }
+        } catch (error) {
+          console.error("Error fetchTotalFunded data:", error);
+        }
+    }
+
+    const handleOpenProcessDialog = () => {
+        if(rowSelectionModel.length !== 1){
+            alert(`한 개의 내역을 선택해주세요.`);
+            return;
+        }
+        setOpenProcessDialog(true);
+    }
+
+    const handleCloseProcessDialog = () => setOpenProcessDialog(false);
+
+    const handleChange = (event) => {
+        console.log("new status: ",event.target.value);
+        setNewStatus(event.target.value);
+    };
+
+    const handleProcess = () => {
+        if(newStatus){
+            let url = `/crowd_order/crowd/purchase/${rowSelectionModel[0]}/update/status`;
+            let method = "PATCH";
+            try{
+                fetch(url, {
+                    method: method,
+                    headers:{
+                        'Authorization': `Bearer ${localStorage.getItem("access_token")}`,
+                        'Content-Type': 'application/json; text=utf-8'
+                    },
+                    body: JSON.stringify({'purchaseStatus': newStatus})
+                }).then(res => {
+                    console.log(res);
+                    if(res.ok) {
+                        alert("업데이트 성공");
+                        handleCloseProcessDialog();
+                        setNewStatus('');
+                        fetchPurchase();
+                    } else {
+                        alert("에러가 발생했습니다.");
+                        setNewStatus('');
+                    }
+                })
+            } catch(error){
+                console.error(error);
+            }
+        } else {
+            alert("유효한 옵션을 선택해주세요.");
+        }
+
+    }
+
+    return(
+        <div>
+            <h2>'{crowd.crowdTitle}'</h2>
+            <h3>펀딩 전체 현황</h3>
+            <p>총 모임 금액 / 목표 금액</p>
+            <p>{totalFunded} / {crowd.crowdGoal}</p>
+            <p>{`${(totalFunded / parseFloat(crowd.crowdGoal) * 100).toFixed(1)}%`}</p>
+            <hr/>
+            <p>리워드별 모금 현황</p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>리워드명</th>
+                        <th>판매량</th>
+                        <th>총 금액 (₩)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {rewardFundedList.map((rewardFunded) => (
+                    <tr key={rewardFunded.rewardId}>
+                        <td>{rewardFunded.rewardName}</td>
+                        <td>{rewardFunded.rewardCounts}</td>
+                        <td>{rewardFunded.rewardSale}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            <hr/>
+            <p>총 결제 건수: {purchaseList.length}</p>
+            <hr/>
+            <p>결제 관리</p>
+            <DataGrid
+                slots={{noRowsOverlay: NoRowsOverlay}}
+                rows={purchaseList}
+                columns={columns}
+                getRowId={(row) => row.purchaseId} 
+                initialState={{
+                    pagination: {
+                        paginationModel: { page: 0, pageSize: 7},
+                    },
+                }}
+                pageSizeOptions={[10, 20]}
+                checkboxSelection
+                onRowSelectionModelChange={(newRowSelectionModel) => {
+                    setRowSelectionModel(newRowSelectionModel);
+                }}
+            />
+            <Button
+                variant="outlined"
+                color="primary"
+                sx={{ mt:2, float:'right' }}
+                onClick={handleOpenProcessDialog}
+            >
+                주문상태 업데이트
+            </Button>
+            <Dialog
+                open={openProcessDialog}
+                onClose={handleCloseProcessDialog}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {rowSelectionModel[0]}번 주문 내역 업데이트
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                    <FormControl sx={{ m: 1, minWidth: 120 }}>
+                        <Select
+                        value={newStatus}
+                        onChange={handleChange}
+                        displayEmpty
+                        inputProps={{ 'aria-label': 'Without label' }}
+                        >
+                        <MenuItem value="">
+                            <em>선택해주세요</em>
+                        </MenuItem>
+                        <MenuItem value={'결제대기'}>결제대기</MenuItem>
+                        <MenuItem value={'결제완료'}>결제완료</MenuItem>
+                        <MenuItem value={'결제취소'}>결제취소</MenuItem>
+                        <MenuItem value={'배송준비'}>배송준비</MenuItem>
+                        <MenuItem value={'배송중'}>배송중</MenuItem>
+                        <MenuItem value={'배송완료'}>배송완료</MenuItem>
+                        </Select>
+                    </FormControl>
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleProcess}>업데이트</Button>
+                </DialogActions>
+            </Dialog>
+
+
+
+        </div>
+    );
+};
+
+export default CrowdManage;
